dfa2c151854df04c190dffdad38db8d171d5d3fe
Jared Hansen <jvhansen08@gmail.com>
2023-05-29 19:48:45 -0500

    pages

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -3,7 +3,7 @@ export function Error() {
         <div style={{display: "flex", justifyContent: "center", padding: "5%"}}>
             <div className="outline-letters">
                 <h1>Hmmm... Something's not right!</h1>
-                <h3>That page doesn't exist; try going to the <a href='/home' style={{color: "White"}}>home page!</a></h3>
+                <h3>That page doesn't exist; try going to the <a href='/me/home' style={{color: "White"}}>home page!</a></h3>
             </div>
         </div>
     )
